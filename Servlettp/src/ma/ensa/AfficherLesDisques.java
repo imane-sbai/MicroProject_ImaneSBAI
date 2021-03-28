3483534eb549c1ae5e9658894b90196ad2934a1d
Imane Sbai <76971234+imane-sbai@users.noreply.github.com>
2021-03-28 23:59:49 +0100

    Add files via upload

---
+++ Servlettp/src/ma/ensa/AfficherLesDisques.java
@@ -0,0 +1,57 @@
+package ma.ensa;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class AfficherLesDisques
+ */
+public class AfficherLesDisques extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public AfficherLesDisques() {
+		super();
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String nom = null;
+		Cookie[] cookies = request.getCookies();
+		nom = Identification.chercheNom(cookies);
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println("<body>");
+		out.println("<head>");
+		out.println("<title> Commande de disques </title>");
+		out.println("</head>");
+		out.println("<body bgcolor=\"white\">");
+		out.println("<h3>" + "Bonjour " + nom + " vous pouvez commander un disque" + "</h3>");
+		Stock.vente(out);
+		out.println("</body>");
+		out.println("</html>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
---
+++ Servlettp/src/ma/ensa/Identification.java
@@ -0,0 +1,35 @@
+package ma.ensa;
+
+import javax.servlet.http.Cookie;
+
+public class Identification {
+
+	public Identification() {
+	}
+
+	static String chercheNom(Cookie[] cookies) {
+		// cherche dans les cookies la valeur de celui qui se nomme "nom"
+		// retourne la valeur de ce nom au lieu de inconnu
+		if (cookies.length > 0) {
+			for (int i = 0; i < cookies.length; i++) {
+				if (cookies[i].getName().equals("nom")) {
+					return cookies[i].getValue();
+				}
+			}
+		}
+		return null;
+	}
+
+	static String chercheMDP(Cookie[] cookies) {
+		// cherche dans les cookies la valeur de celui qui se nomme "motdepasse"
+		// et retourne sa valeur
+		if (cookies.length > 0) {
+			for (int i = 0; i < cookies.length; i++) {
+				if (cookies[i].getName().equals("motdepasse")) {
+					return cookies[i].getValue();
+				}
+			}
+		}
+		return null;
+	}
+}
